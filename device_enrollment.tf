56f2104eac024e6e6183ed320b859e2366abd1c1
skoitoh <shigeki.koito@soliton.co.jp>
2025-08-13 15:16:43 +0900

    Update device_enrollment.tf
    v4tov5migration

--- device_enrollment.tf
+++ device_enrollment.tf
@@ -2,11 +2,9 @@ resource "cloudflare_zero_trust_access_policy" "allow_company_emails" {
   account_id   = var.cloudflare_account_id
   name         = "Allow company emails"
   decision     = "allow"
-  include      = [
-    {
-      email_domain = {
-        domain = "@example.com"
-      }
-    }
-  ]
+  include {
+  email_domain {
+    domain = "example.com"
+  }
+}
 }
